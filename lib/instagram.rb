618c80c77cdb253b314ad3177bd34f54031607d3
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-07-31 11:37:03 -0700

    edit home index to retrieve low_resolution images

--- lib/instagram.rb
+++ lib/instagram.rb
@@ -4,3 +4,7 @@ Instagram.configure do |config|
   config.client_id = "12fcdf7fa7c84507916ec01c7ee73c84"
   config.access_token = "234675123.12fcdf7.5777ff9bf14645be8e2d2bd81c7f3549"
 end
+
+Instagram.images do |image|
+  image.thumbnail
+end
